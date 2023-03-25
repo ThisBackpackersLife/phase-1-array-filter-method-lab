a8c8ec7d8e62aac3b97ffb41949ebab90f2401e3
Nicholas <crazedglitch@yahoo.com>
2023-03-25 17:59:38 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string) {
+    const newDrivers = drivers.filter(person => person.toLowerCase() === string.toLowerCase());
+
+    return newDrivers;
+};
+
+function fuzzyMatch(drivers, string) {
+    const newDrivers = drivers.filter(person => person.startsWith(string));
+    return newDrivers;
+};
+
+// function matchName(drivers, string) {
+//     const newDrivers = drivers.filter(driver => driver === string);
+//     return newDrivers;
+// };
+
+const matchName = (drivers, string) => drivers.filter(person => person.name === string);
